e7ec16764a747786832688d92420526da0fd61ec
HidayatAliMir <meerhidayat563@gmail.com>
2022-09-30 20:45:19 +0530

    Updates

---
+++ precache-manifest.fe9fdee56b88682de30ce7a79f36408d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b4413ec276541f1df19ced51e93bb3",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "d3fb7a62544a19e7dcbc",
+    "url": "/corona-tracker/static/css/main.62008f7a.chunk.css"
+  },
+  {
+    "revision": "ad2737608de56858115f",
+    "url": "/corona-tracker/static/js/2.031001ff.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.031001ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fb7a62544a19e7dcbc",
+    "url": "/corona-tracker/static/js/main.38ad3b43.chunk.js"
+  },
+  {
+    "revision": "2260e51b5f085b0ed802",
+    "url": "/corona-tracker/static/js/runtime-main.6de22c8d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
